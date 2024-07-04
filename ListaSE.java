9f3bb15914b0f248987d5c33aa6bfe57e0fd8ccf
Lívia Mendes <95483204+lvmendess@users.noreply.github.com>
2024-07-04 08:33:56 -0300

    Add files via upload

--- ListaSE.java
+++ ListaSE.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class ListaSE{
     Cell first;
 
@@ -19,6 +21,78 @@ public class ListaSE{
         while(aux!=null){
             System.out.println("Value = "+aux.getValue());
             aux = aux.getNext();
+        }
+    }
+
+    public void insertAtEnd(Cell c){
+        if(empty()){first = c;}
+        else{
+            Cell aux = first;
+            while(aux.getNext() != null){
+                aux = aux.getNext();
+            }
+            aux.setNext(c);
+        }
+    }
+
+    public void insertInMiddle(Cell c) {
+        if(empty()){first = c;}
+        else{
+            Cell aux = first;
+        }
+    }
+
+    public void sort(){
+        //ArrayList sortingList = new ArrayList<Cell>();
+        Cell aux = first;
+        while(aux!=null){
+            
+            aux = aux.getNext();
+        }
+        
+    }
+
+    public Cell search(int v) {
+        if(empty()){return null;}
+        else{
+            Cell aux = first;
+            while(aux != null && aux.getValue() != v){
+                aux = aux.getNext();
+            }
+            return aux;
+        }
+    }
+
+    public boolean removeAtStart() {
+        if(empty()){return false;}
+        else{
+            Cell aux = first;
+            if (aux.getNext() == null) {
+                first = null;
+            } else {
+                first = aux.getNext();
+                aux.setNext(null);
+            }
+            return true;
+        }
+    }
+
+    public boolean removeAtEnd(){
+        if(empty()){return false;}
+        else{
+            Cell ant = first;
+            Cell current = first.getNext();
+            if(current != null){
+                while(current.getNext() != null){
+                    ant = current;
+                    current = current.getNext();
+                }
+                ant.getNext();
+            }else{
+                current = first;
+                first = null;
+            }
+            return true;
         }
     }
     
